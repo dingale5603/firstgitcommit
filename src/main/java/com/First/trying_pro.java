28e5a1b9e347b5b9648ebce0232e90bb019a58dc
dingale5603 <dingale5603@gmail.com>
2022-04-29 19:28:19 +0530

    first git commit

---
+++ src/main/java/com/First/trying_pro.java
@@ -0,0 +1,15 @@
+package com.First;
+
+public class trying_pro {
+    String a;
+    public static void main(String[] args) {
+        System.out.println("deepak is trying first program in intellij");
+        trying_pro obj=new trying_pro();
+        obj.fullname();
+    }
+    void fullname(){
+        a="deepak sudhir ingale";
+        System.out.println("fullname "+a);
+    }
+
+}
